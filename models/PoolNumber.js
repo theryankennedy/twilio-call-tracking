ce028bce69684737df38682e359ccffe69e06d3d
Ryan Kennedy <rkennedy@twilio.com>
2016-11-15 00:06:43 -0800

    number pool

---
+++ models/PoolNumber.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var PoolNumberSchema = new mongoose.Schema({
+  number: {
+    type: String,
+    required: true
+  },
+  pool: {
+    type: String,
+    required: false
+  },
+  status: {
+    type: String,
+    required: false
+  }
+});
+
+// Create a Mongoose model from our schema
+var PoolNumber = mongoose.model('PoolNumber', PoolNumberSchema);
+
+// export model as our module interface
+module.exports = PoolNumber;
